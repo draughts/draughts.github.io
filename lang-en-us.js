bcfcc177820294c569f3568ba14dbaac4e39861a
draughts <32773309+draughts@users.noreply.github.com>
2020-05-20 01:06:16 +0000

    1

--- lang-en-us.js
+++ lang-en-us.js
@@ -24,38 +24,38 @@ var status_cheat = "Cheat";
 // MsgArea messages
 
 var game_over_msg = "Game over!";
-var game_over_no_more_pieces_plyr = "Sorry you lost! Internet Explorer will rule over your computer.";
-var game_over_no_more_pieces_comp = "You won! Internet Explorer no longer rules over your computer.";
+var game_over_no_more_pieces_plyr = "Sorry you lost! Maybe next time. Want to Play Again?";
+var game_over_no_more_pieces_comp = "Congratulations.. You won!";
 var game_over_no_more_moves_plyr = "You have lost, you can not make any moves!";
-var game_over_no_more_moves_comp = "IE has lost, it can not make any moves!";
-var game_over_referee_plyr = "The referee has determined that Firefox is the winner.";
-var game_over_referee_comp = "The referee has determined that Internet Explorer is the winner.";
+var game_over_no_more_moves_comp = "You won! Your opponent can not make any moves!";
+var game_over_referee_plyr = "The referee has determined that you are the winner.";
+var game_over_referee_comp = "The referee has determined that you lost.";
 var game_over_referee_drawn = "The referee has determined that this game has been a draw.";
 
-var your_turn = "It is your turn, click on a piece you wish to move.";
-var wait_for_comp_move = "Please wait for Internet Explorer to make a move.";
-var comp_move = "Internet Explorer is moving a piece.";
-var move_undone = "Previous move undone. It's your turn.";
+var your_turn = "";
+var wait_for_comp_move = "";
+var comp_move = "";
+var move_undone = "";
 
-var select_a_piece = "";
-var select_own_piece = "Please select one of your own pieces.";
-var select_destination = "Click on the square you want to move your piece to.";
+var select_a_piece = "Click on the piece you want to play.";
+var select_own_piece = "";
+var select_destination = "";
 
-var move_not_allowed = "That move is not allowed! Select a piece to play.";
+var move_not_allowed = "";
 var have_to_take_piece = "You have to take your opponent's piece!";
-var complete_your_move = "Complete your move.";
+var complete_your_move = "";
 var must_complete_your_move = "You have to complete your move.";
 
-var not_your_turn = "Hold on, it's not your turn!";
+var not_your_turn = "";
 var game_already_over = "The game is already over!";
-var player_resigned = "Firefox has resigned the game.";
+var player_resigned = "";
 
 // Popup messages
 
 // var referee_warning = "The last 20 moves (10 per player) have only been movements of kings. If no significant change appears within the next 20 moves (10 per player) the referee will decide the winner.";
 var referee_draw = "The referee has determined that this game is a draw.";
-var referee_won_comp = "The referee has determined that this game has been won by Internet Explorer.";
-var referee_won_plyr = "The referee has determined that this game has been won by Firefox.";
+var referee_won_comp = "The referee has determined that this game has been won by the computer.";
+var referee_won_plyr = "The referee has determined that this game has been won by you.";
 var confirm_resign = "Are you sure you want to resign this game?";
 var confirm_resetscores = "Are you sure you want to reset the score?";
 var no_moves_made_yet = "No moves made yet!";
@@ -67,9 +67,9 @@ var ok_button = "OK";
 
 // General
 
-var contributor = "Otto de Voogd";
-var contributor_site = "7is7.com";
-var contributor_url = "http://www.7is7.com/";
+var contributor = "";
+var contributor_site = "";
+var contributor_url = "";
 var loaded_lang_js = 1;
 
 setTimeout('updatePageElements()',100);
